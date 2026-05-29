42449dc3e1e1c807b0efb77276add1470fc1841c
sagnik <sagnik.devv@gmail.com>
2026-05-29 11:42:43 +0530

    [

--- home.js
+++ home.js
@@ -2,29 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // Current Year
     document.getElementById("current-year").textContent = new Date().getFullYear();
 
-    // Theme Toggle Logic
-    const themeToggleBtn = document.getElementById("theme-toggle");
-    const body = document.body;
-
-    const isDark = localStorage.getItem("theme") === "dark";
-
-    if (isDark) {
-        body.classList.replace("light-mode", "dark-mode");
-        themeToggleBtn.innerHTML = '<i class="fas fa-sun"></i>';
-    }
-
-    themeToggleBtn.addEventListener("click", () => {
-        if (body.classList.contains("light-mode")) {
-            body.classList.replace("light-mode", "dark-mode");
-            themeToggleBtn.innerHTML = '<i class="fas fa-sun"></i>';
-            localStorage.setItem("theme", "dark");
-        } else {
-            body.classList.replace("dark-mode", "light-mode");
-            themeToggleBtn.innerHTML = '<i class="fas fa-moon"></i>';
-            localStorage.setItem("theme", "light");
-        }
-    });
-
     // Loading Page Logic
     const sem2Link = document.getElementById("sem2-link");
     const pageLoader = document.getElementById("page-loader");
--- script.js
+++ script.js
@@ -4,39 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("current-year").textContent =
         new Date().getFullYear();
 
-    // Theme Toggle Logic
-    const themeToggleBtn = document.getElementById("theme-toggle");
-    const body = document.body;
-
-    const isDark = localStorage.getItem("theme") === "dark";
-
-    if (isDark) {
-        body.classList.replace("light-mode", "dark-mode");
-        themeToggleBtn.innerHTML = '<i class="fas fa-sun"></i>';
-    }
-
-    themeToggleBtn.addEventListener("click", () => {
-
-        if (body.classList.contains("light-mode")) {
-
-            body.classList.replace("light-mode", "dark-mode");
-
-            themeToggleBtn.innerHTML =
-                '<i class="fas fa-sun"></i>';
-
-            localStorage.setItem("theme", "dark");
-
-        } else {
-
-            body.classList.replace("dark-mode", "light-mode");
-
-            themeToggleBtn.innerHTML =
-                '<i class="fas fa-moon"></i>';
-
-            localStorage.setItem("theme", "light");
-        }
-    });
-
     // SUBJECTS
 
     const commonSubjects = [
